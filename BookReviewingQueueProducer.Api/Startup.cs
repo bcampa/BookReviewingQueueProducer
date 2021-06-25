badc245270e03219c3f348922eea64a4f510e0e2
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-25 17:43:54 -0300

    Configure application startup

--- BookReviewingQueueProducer.Api/Startup.cs
+++ BookReviewingQueueProducer.Api/Startup.cs
@@ -1,3 +1,5 @@
+using BookReviewingQueueProducer.Services.Messaging.Concrete;
+using BookReviewingQueueProducer.Services.Messaging.Contract;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,8 @@ namespace BookReviewingQueueProducer.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSingleton<IMessagePublisher, MessagePublisher>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +34,7 @@ namespace BookReviewingQueueProducer.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
